c4040eb6eda819f7cad7a886c424195ca5694af8
ggzzggzz <71123177+ggzzggzz@users.noreply.github.com>
2022-04-21 12:32:50 +0900

    0421 middle

---
+++ src/chap20/ServletContextListener02.java
@@ -0,0 +1,35 @@
+package chap20;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+/**
+ * Application Lifecycle Listener implementation class ServletContextListener02
+ *
+ */
+@WebListener
+public class ServletContextListener02 implements ServletContextListener {
+
+    /**
+     * Default constructor. 
+     */
+    public ServletContextListener02() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+     * @see ServletContextListener#contextDestroyed(ServletContextEvent)
+     */
+    public void contextDestroyed(ServletContextEvent sce)  { 
+    	System.out.println("두번째 jsp 앱 종료됨####");
+    }
+
+	/**
+     * @see ServletContextListener#contextInitialized(ServletContextEvent)
+     */
+    public void contextInitialized(ServletContextEvent sce)  { 
+    	System.out.println("두번째 jsp 앱 시작됨$$$$");
+    }
+	
+}
