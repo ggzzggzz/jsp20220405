a1dca8cfccc78af2733169ef32859a979509b99a
ggzzggzz <71123177+ggzzggzz@users.noreply.github.com>
2022-04-27 17:19:51 +0900

    0427

---
+++ WebContent/WEB-INF/sql/15HAVING.sql
@@ -0,0 +1,29 @@
+USE w3schools;
+
+SELECT * FROM Customers;
+
+SELECT Country, COUNT(CustomerID) count FROM Customers
+WHERE count = 6 -- WHERE FROM절 테이블의 각 ROW를 연산
+GROUP BY Country
+;
+
+SELECT Country, COUNT(CustomerID) FROM Customers
+GROUP BY Country
+HAVING COUNT(CustomerID) > 3; -- HAVING aggregate function 결과를 조건으로 줄 수 있음
+
+SELECT Country, COUNT(CustomerID) count FROM Customers
+GROUP BY Country
+HAVING count > 3;
+
+-- 가장 적은 고객이 있는 나라들 조회
+SELECT Country, COUNT(CustomerID) count FROM Customers
+GROUP BY Country
+HAVING count = 
+(
+	SELECT COUNT(CustomerID) count FROM Customers
+    GROUP BY Country
+    ORDER BY count
+    LIMIT 1
+);
+
+
